c7404d73abf0680f4c59f80ea3e91193cd09dd93
upcode maquina1 <upcode maquina1>
2016-11-30 21:42:03 -0200

    tela de login

---
+++ src/pages/home/home.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  private usuario = {};
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  entrar(){
+
+  }
+
+  abrirCadastro(){
+    
+  }
+
+}
