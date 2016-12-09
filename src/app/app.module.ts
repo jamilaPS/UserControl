4029eb53ddfca04b6e0407b22db0104325065a6e
upcode maquina1 <upcode maquina1>
2016-12-09 15:41:32 -0200

    Tela de cadastro e correção do CORS filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { CadastroUsuarioPage } from '../pages/cadastro-usuario/cadastro-usuario';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    CadastroUsuarioPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +16,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    CadastroUsuarioPage
   ],
   providers: []
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,25 +1,47 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { CadastroUsuarioPage } from '../cadastro-usuario/cadastro-usuario';
+import { AlertController } from 'ionic-angular';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  cadastroUsuarioPage = CadastroUsuarioPage;
   private usuario = {};
+  private url = "http://localhost:8100/userControl/usuario/entrar";
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public http: Http, public alertCtrl: AlertController) {
   }
 
   entrar(){
+    this.http.post(this.url, this.usuario).map(res => res.json()).subscribe(data => {
+      console.log("chegou aqui!");
+      console.log(data);
+      if(data.status == 401){
+        this.showAlert('Atenção!', 'Email ou senha incorretos.')
+      }
+      else if(data.status == 200){
+        console.log("chegou aqui");
+        console.log(data);
+         window.sessionStorage.setItem('usuarioLogado', JSON.stringify(data));
+       }
+     else{
+       this.showAlert('Atenção!', 'Ocorreu um erro, por favor tente novamente.')
+     }
+    });
+   }
 
-  }
-
-  abrirCadastro(){
-    
-  }
-
+   private showAlert(titulo: string, mensagem: string) {
+     let alert = this.alertCtrl.create({
+       title: titulo,
+       subTitle: mensagem,
+       buttons: ['OK']
+     });
+     alert.present();
+   }
 }
